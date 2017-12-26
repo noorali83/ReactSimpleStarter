79b1f1e2dc7c320fce4e6724fa97f4c78b9b1ac3
Noor Ali <noorali83@gmail.com>
2017-12-27 10:25:09 +1100

    Initial commit to show search bar and its dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const Videos = () => {
+  return ( <div>
+  			<div> my Videos </div>
+  	        <div> <SearchBar /> </div> 
+  	       </div>
+  	);
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render( < Videos / > , document.querySelector(".container"));
