beb2a7f77b5b5ea593d6789b3aa702ff5c31f547
Noor Ali <noorali83@gmail.com>
2017-12-28 12:24:26 +1100

    Added defualt search text

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input / > ;
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "Search"
+    }
+  }
+
+  render() {
+    var searchText = this.state.searchValue;
+    return (< div > < input type = "text" value = {this.state.searchValue }> < /input>< /div >
+    );
+  }
+
 }
 
 export default SearchBar;
